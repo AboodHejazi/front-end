317b0c1f661053d341222aca7b9eb2c7960f038e
AboodHejazi <165851510+AboodHejazi@users.noreply.github.com>
2024-11-06 23:45:01 +0200

    Add files via upload

---
+++ blogsite-project/subscibejs/subscribe.js
@@ -0,0 +1,8 @@
+
+let subscribeButton = document.querySelector(".submit-email");
+let emailInput = document.querySelector(".add-email");
+
+
+subscribeButton.onclick = function() {
+    emailInput.value = ""; 
+};
