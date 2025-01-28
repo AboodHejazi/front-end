5d6ceec4dba5b424b3d1f0b8f185af23579eb2ae
AboodHejazi <165851510+AboodHejazi@users.noreply.github.com>
2025-01-28 13:43:38 +0200

    Delete starter_project directory

--- starter_project/tests/api.test.js
+++
@@ -1,3 +0,0 @@
-test('API should return 200 on successful request', () => {
-    expect(true).toBe(true); 
-  });
--- starter_project/tests/formHandler.test.js
+++
@@ -1,8 +0,0 @@
-const { checkFormInput } = require('../src/client/js/formHandler');
-test('Valid URL should return true', () => {
-    expect(checkFormInput('https://valid-url.com')).toBe(true);
-  });
-  
-  test('Invalid URL should return false', () => {
-    expect(checkFormInput('invalid-url')).toBe(false);
-  });
